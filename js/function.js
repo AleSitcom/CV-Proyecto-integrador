254aca3dd5ff95a026bad751cb2323fd3eb6d6c2
Alesitcom <alesitcom@gmail.com>
2023-03-08 12:20:50 -0300

    Primer esccritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,11 @@
+Document.addEventListener{"click", function() {
+    console.log("Hola mundo desde EventListener");
+    document,getElementById("demo")innerHTML = "Estamos probando nuestro primer evento"
+
+
+});
+
+document.getElementById("boton").addEventListener("click", function() { 
+    console.log("Hola mundo desde EventListener");
+    document,getElementById("demo")innerHTML = "Estamos probando nuestro primer evento"
+  })
